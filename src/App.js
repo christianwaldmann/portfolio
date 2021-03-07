b202fb3e8bff41ec5dd8ac98f494a77385733780
christianwaldmann <waldmann-christian@web.de>
2021-03-07 13:57:05 +0100

    Fix bug where scroll position is not at top on page navigation

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Error404NotFound from "./pages/Error404NotFound";
 import { Helmet } from "react-helmet";
 import { ThemeProvider } from "./context/Theme";
 import { projects } from "./config/projects";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
 	return (
 		<Router basename={process.env.PUBLIC_URL}>
+			<ScrollToTop />
 			<ThemeProvider>
 				<Helmet>
 					<body className="bg-white dark:bg-gray-900"></body>
